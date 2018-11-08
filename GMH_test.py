36460bec05945a80476847d52653b415289ffaeb
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2018-11-08 16:16:39 +1300

    PEP8 tidy-up and Open() impovements
    Ensure any positive return value of GMH_OpenCom() is interpreted as 'Success'. Automatically report return code AND message.

--- GMH_test.py
+++ GMH_test.py
@@ -5,25 +5,25 @@ Created on Fri Feb 24 16:58:44 2017
 @author: t.lawson
 """
 
-PORT = 6 # Change this to whatever port Windows assigns to your device
-
 import GMHstuff as GMH
 
-GFTB200 = GMH.GMH_Sensor(PORT, demo = False)
-print '\nSensor info:\n',GFTB200.info,'\n'
+PORT = 4  # Change this to whatever port Windows assigns to your device
+
+GFTB200 = GMH.GMH_Sensor(PORT, demo=False)
+print '\nSensor info:\n', GFTB200.info, '\n'
 T = GFTB200.Measure('T')
-print T[0],T[1]
+print T[0], T[1]
 P = GFTB200.Measure('P')
-print P[0],P[1]
+print P[0], P[1]
 RH = GFTB200.Measure('RH')
-print RH[0],RH[1]
+print RH[0], RH[1]
 T_dew = GFTB200.Measure('T_dew')
-print T_dew[0],T_dew[1]
+print T_dew[0], T_dew[1]
 T_wb = GFTB200.Measure('T_wb')
-print T_wb[0],T_wb[1]
+print T_wb[0], T_wb[1]
 H_abs = GFTB200.Measure('H_abs')
-print H_abs[0],H_abs[1]
+print H_abs[0], H_abs[1]
 H_atm = GFTB200.Measure('H_atm')
-print H_atm[0],H_atm[1]
+print H_atm[0], H_atm[1]
 
 GFTB200.Close()
