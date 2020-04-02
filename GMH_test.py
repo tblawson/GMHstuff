1ddd7fa08e4c4f8591e7a40c3172a7ba474b8cbd
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2020-04-02 18:55:54 +1300

    Added all remaining useful functions and ensured consistent treatment of errors / exceptions
    This is now DEFINATELY considered the Working Version!

--- GMH_test.py
+++ GMH_test.py
@@ -16,11 +16,6 @@ GFTB200 = GMH.GMHSensor(PORT)
 GFTB200.open_port()
 # print('\ndemo status:', GFTB200.demo)  # 'demo = False' means a fully-functional device
 
-
-# for i in range(GFTB200.chan_count):
-#     v = GFTB200.get_meas_attributes(i)
-#     print('{}: in {} (ch{})'.format(i, v[1], v[0]))
-
 print('\nBelow is a list of available measurements for this device:')
 print('\n{:>20}{:>5}{:>10}{:>15}{:>15}'.format('quantity', 'chan', 'value', 'unit_from_info', 'unit_from_meas'))
 for q in GMH.MEAS_ALIAS.keys():
@@ -31,7 +26,18 @@ for q in GMH.MEAS_ALIAS.keys():
     meas_unit = measurement[1]
     print('{:>20}{:>5}{:>10}{:>15}{:>15}'.format(quantity, attribs[0], val, attribs[1], meas_unit))
 
-print(GFTB200.get_meas_attributes('height'))
+print('\nMeas attributes:',GFTB200.get_meas_attributes('P'))
+
+print('\nTemperature:', GFTB200.measure('T'))
+
+print('\nDisp unit:', GFTB200.get_disp_unit(3))
+
+print('\nPower off time: {} mins'.format(GFTB200.get_power_off_time()))
+print('\nSet power off time to {} mins:'.format(GFTB200.set_power_off_time(1)))
+print('\nPower off time: {} mins'.format(GFTB200.get_power_off_time()))
+
+vers, id = GFTB200.get_sw_info()
+print('\nSoftware info: v{}.{}'.format(vers, id))
 
 GFTB200.get_sensor_info()
 
